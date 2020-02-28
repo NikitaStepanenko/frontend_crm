b40889127d96d310a31ac9f6686a9f96c5bc4493
Rodion <rodion_leginkov@mail.ru>
2020-02-28 18:16:23 +0300

    Add Sign in sign up without jwt token

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/NavBar/Navbar';
 import Users from './Pages/UsersPage/Users';
 import Projects from './Pages/ProjectsPage/Projects';
 import Home from './Pages/HomePage/Home';
+import Admin from "./Pages/AdminPage/Admin.jsx"
 import { createMuiTheme } from '@material-ui/core/styles';
-
+import SignUp from "./Pages/SignUnPage/SignUp.jsx"
+import Signin from "./Pages/SignInPage/SignIn.jsx";
 const theme = createMuiTheme({
   palette: {
     primary: { main: '#32418C' },
@@ -14,6 +16,7 @@ const theme = createMuiTheme({
 
 function App() {
   return (
+
     <BrowserRouter>
       <div className="App" theme={theme}>
         <Navbar />
@@ -21,6 +24,10 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/users" component={Users} />
           <Route exact path="/projects" component={Projects} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/admin" component={Admin} />
+
         </Switch>
       </div>
     </BrowserRouter>
